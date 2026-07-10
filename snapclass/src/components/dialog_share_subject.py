d9bbbd14caea77237fa4cebd7c62be2ceb7fb0c0
shwetakanak <shwetakanak@users.noreply.github.com>
2026-07-10 19:45:31 +0530

    Finish Changes

--- snapclass/src/components/dialog_share_subject.py
+++ snapclass/src/components/dialog_share_subject.py
@@ -4,7 +4,7 @@ import segno
 import io
 @st.dialog('Create New Subject')
 def share_subject_dialog(subject_name,subject_code):
-    app_domain="snapclass-byshweta.streamlit.app"
+    app_domain="snapclass-byshwetakanak.streamlit.app"
     join_url = f"{app_domain}/?join-code={subject_code}"
     st.header("Scan to Join")
     qr=segno.make(join_url)
