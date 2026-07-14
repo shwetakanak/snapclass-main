f59fd4f1abd670b28b58399478e675cb1b8c787b
shwetakanak <shwetakanak@users.noreply.github.com>
2026-07-14 12:03:49 +0530

    Final Commit

--- snapclass/src/ui/base_layout.py
+++ snapclass/src/ui/base_layout.py
@@ -5,12 +5,11 @@ def style_background_home():
     st.markdown(
         """
         <style>
-        .stApp {
+        .stApp{
             background: #5865F2 !important;
-            color: #000000 !important;
         }
 
-        .stApp div[data-testid="stColumn"] {
+        .stApp div[data-testid="stColumn"]{
             background-color: #E0E3FF !important;
             padding: 2.5rem !important;
             border-radius: 5rem !important;
@@ -25,9 +24,8 @@ def style_background_dashboard():
     st.markdown(
         """
         <style>
-        .stApp {
+        .stApp{
             background: #E0E3FF !important;
-            color: #000000 !important;
         }
         </style>
         """,
@@ -40,68 +38,41 @@ def style_base_layout():
         """
         <style>
 
-        /* Google Fonts */
         @import url('https://fonts.googleapis.com/css2?family=Climate+Crisis&display=swap');
         @import url('https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap');
 
-        /* Hide Streamlit default UI */
+        /* Hide Streamlit top bar */
         #MainMenu,
         header,
-        footer {
+        footer{
             visibility: hidden;
         }
 
-        .block-container {
+        .block-container{
             padding-top: 1.5rem !important;
         }
 
-        /* Global text color */
-        .stApp {
-            color: #000000 !important;
-        }
-
-        /* Headings */
-        h1 {
+        h1{
             font-family: 'Climate Crisis', sans-serif !important;
             font-size: 2rem !important;
             line-height: 1.1 !important;
             margin: 0 !important;
-            color: #000000 !important;
         }
 
-        h2 {
+        h2{
             font-family: 'Climate Crisis', sans-serif !important;
             font-size: 2rem !important;
             line-height: 0.9 !important;
             margin: 0 !important;
-            color: #000000 !important;
         }
 
-        /* Normal text */
         h3,
         h4,
-        h5,
-        h6,
-        p,
-        span,
-        div,
-        label,
-        li,
-        td,
-        th {
+        p{
             font-family: 'Outfit', sans-serif !important;
-            color: #000000 !important;
         }
 
-        /* Text input */
-        input,
-        textarea {
-            font-family: 'Outfit', sans-serif !important;
-            color: #000000 !important;
-        }
-
-        /* Buttons */
-        button {
+        button{
             border-radius: 1.5rem !important;
             background-color: #5865F2 !important;
             color: white !important;
@@ -110,25 +81,25 @@ def style_base_layout():
             transition: transform 0.25s ease-in-out !important;
         }
 
-        button[kind="secondary"] {
+        button[kind="secondary"]{
+            border-radius: 1.5rem !important;
             background-color: #EB459E !important;
             color: white !important;
             padding: 10px 20px !important;
-            border-radius: 1.5rem !important;
             border: none !important;
             transition: transform 0.25s ease-in-out !important;
         }
 
-        button[kind="tertiary"] {
+        button[kind="tertiary"]{
+            border-radius: 1.5rem !important;
             background-color: black !important;
             color: white !important;
             padding: 10px 20px !important;
-            border-radius: 1.5rem !important;
             border: none !important;
             transition: transform 0.25s ease-in-out !important;
         }
 
-        button:hover {
+        button:hover{
             transform: scale(1.05);
         }
 
